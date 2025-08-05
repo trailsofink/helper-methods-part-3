b77b88095e190452996bdb85ebfc0eee5026420a
trailsofink <trailsofink@gmail.com>
2025-08-04 20:48:17 -0500

    adding bootstrap and bootstrap nav.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_13_202311) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_02_205042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.string "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
